7c8c02a4c319560e9c58e22ced8decf53bb65cc5
YashAgarwal1201 <legoyashx@gmail.com>
2024-11-11 20:21:15 +0530

    chore: index page

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="w-screen h-screen">
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <Layout>
+    <div class="w-full h-full flex justify-center items-center">
+      <h1 class="text-2xl md:text-3xl capitalize">coming soon</h1>
+    </div></Layout
+  >
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
