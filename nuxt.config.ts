08411225e37f0fd4ea8eb8c26080b5291be64d23
YashAgarwal1201 <legoyashx@gmail.com>
2024-11-11 20:06:51 +0530

    chore: nuxt setup

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+
+  devServer: {
+    port: 5176,
+    host: "0.0.0.0",
+  },
+
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: {
+      name: "slide",
+      mode: "out-in",
+    },
+  },
+
+  modules: ["@nuxt/ui"],
+});
