65d6a9c6e046d25f7ca3e73a990ec7a0db81680c
YashAgarwal1201 <legoyashx@gmail.com>
2025-04-19 20:20:49 +0530

    refactor: streamline local development server configuration and clean up unused code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-// import { definePreset } from "@primeuix/themes";
-// import Aura from "@primeuix/themes/aura";
 import MyPreset from "./MyPreset";
 import tailwindcss from "@tailwindcss/vite";
 import fs from "fs";
+
+const isLocalDev = process.env.NODE_ENV === "development";
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
@@ -12,13 +11,21 @@ export default defineNuxtConfig({
   devServer: {
     port: 5176,
     host: "0.0.0.0",
-    https: {
-      key: fs.readFileSync("./localhost-key.pem", "utf8"),
-      cert: fs.readFileSync("./localhost.pem", "utf8"),
-    },
+    ...(isLocalDev &&
+    fs.existsSync("./localhost-key.pem") &&
+    fs.existsSync("./localhost.pem")
+      ? {
+          https: {
+            key: fs.readFileSync("./localhost-key.pem", "utf8"),
+            cert: fs.readFileSync("./localhost.pem", "utf8"),
+          },
+        }
+      : {}),
   },
+
   css: ["~/assets/css/main.css"],
   ssr: false, // Disable server-side rendering (hack)
+
   app: {
     pageTransition: { name: "page", mode: "out-in" },
     layoutTransition: { name: "slide", mode: "out-in" },
@@ -34,20 +41,13 @@ export default defineNuxtConfig({
   },
 
   modules: [
-    // "@nuxt/ui",
     "@pinia/nuxt",
     "@nuxt/fonts",
     "@vite-pwa/nuxt",
     "@primevue/nuxt-module",
   ],
-  plugins: [
-    { src: "~/plugins/highcharts-vue.ts", mode: "client" }, // Only include on the client
-  ],
 
-  // tailwindcss: {
-  //   configPath: "tailwind.config.ts", // ✅ Reference the external Tailwind config
-  //   quiet: true, // ✅ Suppresses warnings
-  // },
+  plugins: [{ src: "~/plugins/highcharts-vue.ts", mode: "client" }],
 
   fonts: { families: [{ name: "Raleway", provider: "google" }] },
 
@@ -68,19 +68,15 @@ export default defineNuxtConfig({
         },
       ],
     },
-    // workbox: {
-    //   navigateFallback: "/",
-    //   globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-    // },
     client: {
       installPrompt: true,
-      // Prompt PWA install on supported browsers
     },
     devOptions: {
       enabled: true,
       type: "module",
     },
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
